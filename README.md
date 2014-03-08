af66ff3f9b51785f2d76b029cca454f2fda0a260
Daniel Serrano <danieljdserrano@gmail.com>
2014-03-08 12:29:21 +0000

    blog with post entries

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ objects-on-rails
 ================
 
 rails tutorial from http://objectsonrails.com/
+
+i'm on [page 17](http://objectsonrails.com/#sec-6)
---
+++ blog-tutorial/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogTutorial::Application.config.secret_key_base = 'e1feb88cfa37f9e2df723a9f05cdc6c1cfeac3023fdd0933c2c91f7336b2a78eae2dcb81c09e08632594f55ea36d063abcc2d9ccbc0653c8f7c0c0af34421a4a'
